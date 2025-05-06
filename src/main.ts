6791b200b62df5523fed9a85a83ecd369a0d04b3
Mohit bhardwaj <82044623+mohitbhardwaji@users.noreply.github.com>
2025-05-06 18:28:35 +0530

    proxy cors fix

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{ logger: ['log', 'error', 'warn', 'debug', 'verbose'] });
   app.enableCors({
-    origin: 'http://localhost:5173', // frontend Vite dev server
-    credentials: true, // allow cookies if you use them
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen( 3000 , '0.0.0.0');
 }
 bootstrap();
