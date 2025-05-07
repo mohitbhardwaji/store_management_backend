a5ef6cf937bb146c5059dde1f630315a8565af44
Kunal Saxena <kunalsaxena@Mac.lan>
2025-05-08 01:31:01 +0530

    Stop tracking dist folder

--- dist/dto/bill.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateBillDto = void 0;
-class CreateBillDto {
-    products;
-    totalAmount;
-}
-exports.CreateBillDto = CreateBillDto;
-//# sourceMappingURL=bill.dto.js.map
